daadc43759aad8c9d9af06ab03a5c352d60094d9
nelynely <nelly.nf54@gmail.com>
2025-04-25 15:18:33 -0300

    Add other files

---
+++ src/exercise-types.js
@@ -0,0 +1,10 @@
+let nome, idade, animado;
+
+//Informe seu código aqui
+nome = "Omar Marmoush";
+idade = 26;
+animado = true;
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof animado);
