337f7ff498de70669c26e425b911c852012a7b55
nelynely <nelly.nf54@gmail.com>
2025-04-25 15:13:53 -0300

    Create boolean.js

---
+++ src/boolean.js
@@ -0,0 +1,14 @@
+// boolean - possuem apenas dois valores: verdadeiro ou falso
+let isActive = false;
+
+console.log(isActive);
+console.log(typeof isActive);
+
+/*
+Boolean - possuem apenas dois valores: verdadeiro ou falso;
+Undefined - indica que não foi definido um valor;
+Null - indica que um valor é nulo;
+Number - armazena valores numéricos;
+String - armazena textos;
+Symbol - armazena símbolos. Seu valor é sempre único e imutável
+*/
