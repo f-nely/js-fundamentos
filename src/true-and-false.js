312efa00b44fce23b09bda364138c6e41b9f87b5
nelynely <nelly.nf54@gmail.com>
2025-04-25 15:16:24 -0300

    Create true-and-false.js

---
+++ src/true-and-false.js
@@ -0,0 +1,16 @@
+/*
+Valores considerados Verdadeiros e Falsos
+Os seguintes valores são considerados falsos no JavaScript:
+* 0;
+* -0;
+* null;
+* false;
+* NaN;
+* undefined;
+* "" (string vazia).
+Qualquer outro valor fora dessa lista é considerado true
+*/
+
+let bool = "";
+
+bool == true ? console.log("true") : console.log("false");
