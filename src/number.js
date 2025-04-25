62e5858081e82fa980f45131361ebeb1c9679f82
nelynely <nelly.nf54@gmail.com>
2025-04-25 15:14:55 -0300

    Create number.js

---
+++ src/number.js
@@ -0,0 +1,9 @@
+// number - armazena valores numéricos
+let amountOfTrophies = 36;
+
+console.log(
+  `How many trophies has Man City won in total? It's ${amountOfTrophies}`
+);
+
+let notacaoCientifica = 2.5e2; // 2.5 * 10²
+console.log(notacaoCientifica);
