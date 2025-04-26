4cebb47a9ee95992f019ed2fb0c7b48e1a8a41bf
nelynely <nelly.nf54@gmail.com>
2025-04-26 13:31:27 -0300

    Create variables.js

---
+++ src/variables.js
@@ -0,0 +1,22 @@
+/** JavaScript is a "dynamically typed language"
+ * you don't need to specify what data type a variable will contain (numbers, strings, etc.)
+ * var (escopo global ou de função), let (escopo de bloco) e const (constantes com valor imutável)
+ */
+
+let stadium = "Etihad Stadium";
+let club = "Manchester City";
+let points = 61;
+let isQualifyToChampionsLeague = true;
+
+console.log(stadium);
+console.log(club);
+console.log(points);
+console.log(isQualifyToChampionsLeague);
+console.log(typeof stadium);
+
+/** Constants in JavaScript
+ * you must initialize them when you declare them.
+ * you can't assign them a new value after you've initialized them.
+ */
+const count = 1;
+console.log(count);
