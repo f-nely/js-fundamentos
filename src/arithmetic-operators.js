44dbcb08534522583ba1aaa06080767e148882eb
nelynely <nelly.nf54@gmail.com>
2025-04-26 13:26:09 -0300

    Create arithmetic-operators.js

---
+++ src/arithmetic-operators.js
@@ -0,0 +1,9 @@
+//Arithmetic operators
+let n1 = 5;
+let n2 = 2;
+console.log(`${n1} + ${n2} = ${n1 + n2}`); // Addition
+console.log(`${n1} - ${n2} = ${n1 - n2}`); // Subtraction
+console.log(`${n1} * ${n2} = ${n1 * n2}`); // Multiplication
+console.log(`${n1} / ${n2} = ${n1 / n2}`); // Division
+console.log(`${n1} % ${n2} = ${n1 % n2}`); // Remainder (sometimes called modulo)
+console.log(`${n1} ** ${n2} = ${n1 ** n2}`); // Exponent
