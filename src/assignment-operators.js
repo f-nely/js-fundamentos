609b527ee902d82d9dffe9ccdbcdce025eb2cf69
nelynely <nelly.nf54@gmail.com>
2025-04-26 13:26:53 -0300

    Create assignment-operators.js

---
+++ src/assignment-operators.js
@@ -0,0 +1,12 @@
+let x = 3; // x contains the value 3
+let y = 4; // y contains the value 4
+x = y; // x now contains the same value y contains, 4
+
+x += 4; // x = x + 4;
+console.log(x); // 8
+x -= 3; // x = x - 3;
+console.log(x); // 5
+x *= 3; // x = x * 3;
+console.log(x); // 15
+x /= 5; // x = x / 5;
+console.log(x); // 3
