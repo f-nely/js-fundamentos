0dffb3c3e10a0aba39313673a419aac5d1d294c0
nelynely <nelly.nf54@gmail.com>
2025-04-26 13:29:20 -0300

    Create increment-decrement-operator.js

---
+++ src/increment-decrement-operator.js
@@ -0,0 +1,12 @@
+let guessCount = 99;
+guessCount++; // Post Increment
+console.log(guessCount);
+
+let trophies = 61;
+++trophies; // Pre Increment
+console.log(trophies);
+
+let num = 11;
+num--; // Post Decrement
+console.log(num);
+console.log(--num); // Pre Decrement
