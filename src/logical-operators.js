a020498b8500641d79fae28de475f76d5c67883e
nelynely <nelly.nf54@gmail.com>
2025-04-26 13:30:14 -0300

    Create logical-operators.js

---
+++ src/logical-operators.js
@@ -0,0 +1,11 @@
+// Logical AND (&&) operator
+let x = 6;
+let y = 3;
+console.log(x < 10 && y > 1); // true
+
+// Logical OR (||) operator
+console.log(x == 5 || y == 5); // false
+
+// Logical NOT (!) operator
+console.log(!(x == y)); // true
+/** The logical NOT (!) (logical complement, negation) operator takes truth to falsity and vice versa.  */
