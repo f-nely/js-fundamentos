380457e69c8f181855199cf6a90d648402b8b492
nelynely <nelly.nf54@gmail.com>
2025-04-28 15:44:33 -0300

    Create ex-amount-of-orange.js

---
+++ src/ex-amount-of-orange.js
@@ -0,0 +1,7 @@
+let totalLaranjas = 4;
+let quantidadeComida = 1;
+let resultado;
+
+//console.log(((totalLaranjas - quantidadeComida) / totalLaranjas) * 100);
+resultado = 100 - (quantidadeComida * 100) / totalLaranjas;
+console.log(resultado);
