1ad54f22d311a65dd7ad8a75d2fa7f36711a4f4e
nelynely <nelly.nf54@gmail.com>
2025-04-30 18:30:12 -0300

    Create switch.js

---
+++ src/switch.js
@@ -0,0 +1,45 @@
+let month = -10,
+  nameMonth = "";
+
+switch (month) {
+  case 1:
+    nameMonth = "January";
+    break;
+  case 2:
+    nameMonth = "February";
+    break;
+  case 3:
+    nameMonth = "March";
+    break;
+  case 4:
+    nameMonth = "April";
+    break;
+  case 5:
+    nameMonth = "May";
+    break;
+  case 6:
+    nameMonth = "June";
+    break;
+  case 7:
+    nameMonth = "July";
+    break;
+  case 8:
+    nameMonth = "August";
+    break;
+  case 9:
+    nameMonth = "September";
+    break;
+  case 10:
+    nameMonth = "October";
+    break;
+  case 11:
+    nameMonth = "November";
+    break;
+  case 12:
+    nameMonth = "December";
+    break;
+  default:
+    nameMonth = "Invalid month";
+}
+
+console.log(nameMonth);
