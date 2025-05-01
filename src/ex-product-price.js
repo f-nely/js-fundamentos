b759d534545595073a37b1d315e02ade70a9011d
nelynely <nelly.nf54@gmail.com>
2025-05-01 10:09:43 -0300

    Create ex-product-price.js

---
+++ src/ex-product-price.js
@@ -0,0 +1,14 @@
+let quantidade = 35;
+
+let price = 5.5;
+if (quantidade >= 25) {
+  price = 4.85;
+  console.log(price * quantidade);
+} else {
+  console.log(price * quantidade);
+}
+
+/**
+ * let preco = quantidade < 25 ? 5.5 : 4.85;
+ * console.log(preco * quantidade);
+ */
