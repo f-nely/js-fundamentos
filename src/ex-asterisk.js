ea20ea7e04e2587659cad17733276c1383c913f1
nelynely <nelly.nf54@gmail.com>
2025-05-08 10:42:28 -0300

    Create ex-asterisk.js

---
+++ src/ex-asterisk.js
@@ -0,0 +1,18 @@
+let numero = 5;
+
+/* let asterisk = "";
+function printAsterisk(num) {
+  for (let i = 1; i <= num; i++) {
+    asterisk += "*";
+  }
+  console.log(asterisk);
+}
+
+printAsterisk(numero); */
+
+let a = "";
+
+for (let i = 1; i <= numero; i++) {
+  a += "*";
+}
+console.log(a);
