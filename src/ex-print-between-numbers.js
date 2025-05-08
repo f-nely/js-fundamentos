1c884b32d6d4b8d54d9b2f720da73cd8b4617e69
nelynely <nelly.nf54@gmail.com>
2025-05-08 10:41:14 -0300

    Create ex-print-between-numbers.js

---
+++ src/ex-print-between-numbers.js
@@ -0,0 +1,12 @@
+let inicio = 5;
+let fim = 2;
+
+if (fim > inicio) {
+  for (let i = inicio; i <= fim; i++) {
+    console.log(i);
+  }
+} else {
+  for (let j = inicio; j >= fim; j--) {
+    console.log(j);
+  }
+}
